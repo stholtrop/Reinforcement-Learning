d14120e733184d5cf3ad06a4fb267cf9cb886924
lucashc <lucascrijns@gmail.com>
2018-11-22 15:10:52 +0100

    Matrix attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <cmath>
+#include "matrix.cpp"
 
 
 using namespace std;
 
 int main(){
-  cout << "Hello world" << endl;
+  auto m1 = new Matrix<float>(2,2);
+  cout << m1[2];
 }
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,8 +1,18 @@
+#include <vector>
 #include <cmath>
 
 template <class T>
 class Matrix {
+private:
+  std::vector<T> data;
+  int rows;
+  int columns;
   Matrix(int x, int y) {
-    
+    data(x * y);
+    rows = x;
+    columns = y;
   }
-}
+  T& operator[](int x){
+    return &data[x];
+  }
+};

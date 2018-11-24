32c26984d3542df3a757499e14b996bd44cd163b
lucashc <lucascrijns@gmail.com>
2018-11-24 14:04:46 +0100

    Activations are now fully working

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,7 @@
 using namespace std;
 
 int main(){
-  Matrix<float> m1 = Matrix<float>(2, 2, {1,2,3,4});
-  Matrix<float> m2 = Activator<float>::activation(m1, Activation::SIGMOID);
-  cout << m1 << endl;
-  cout << m2 << endl;
+  Matrix<float> m1 = Matrix<float>(2, 1, {1,2});
   NeuralNetwork<float> nn({2, 6, 1}, Activation::SIGMOID);
   auto m3 = nn.evaluate(m1);
   cout << m3 << endl;
--- src/neural-network.cpp
+++ src/neural-network.cpp
@@ -6,58 +6,58 @@
 #include "approx.cpp"
 
 enum Activation {
-  TANH, SIGMOID
+	TANH, SIGMOID
 };
 
 template <typename T>
 class Activator {
 public:
 
-  static Matrix<T> activation(Matrix<T> m, Activation type) {
-    auto func = [&type](T x){
-      return activation(x, type);
-    };
-    return m.apply(func);
-  }
+	static Matrix<T> activation(Matrix<T> m, Activation type) {
+		auto func = [&type](T x){
+			return activation(x, type);
+		};
+		return m.apply(func);
+	}
 
-  static Matrix<T> activationDerivative(Matrix<T> m, Activation type) {
-    auto func = [&type](T y){
-      return activationDerivative(y, type);
-    };
-    return m.apply(func);
-  }
+	static Matrix<T> activationDerivative(Matrix<T> m, Activation type) {
+		auto func = [&type](T y){
+			return activationDerivative(y, type);
+		};
+		return m.apply(func);
+	}
 
-  static T activation(T x, Activation type) {
-    switch (type){
-      case TANH: return tanH(x);
-      case SIGMOID: return sigmoid(x);
-      default: return sigmoid(x);
-    };
-  }
+	static T activation(T x, Activation type) {
+		switch (type){
+			case TANH: return tanH(x);
+			case SIGMOID: return sigmoid(x);
+			default: return sigmoid(x);
+		};
+	}
 
-  static T activationDerivative(T y, Activation type) {
-    switch (type) {
-      case TANH: return tanHPrime(y);
-      case SIGMOID: return sigmoidPrime(y);
-      default: return sigmoidPrime(y);
-    }
-  }
+	static T activationDerivative(T y, Activation type) {
+		switch (type) {
+			case TANH: return tanHPrime(y);
+			case SIGMOID: return sigmoidPrime(y);
+			default: return sigmoidPrime(y);
+		}
+	}
 
-  static inline T sigmoid(T x) {
-    return 1 / (1 + std::exp(-x));
-  }
+	static inline T sigmoid(T x) {
+		return 1 / (1 + std::exp(-x));
+	}
 
-  static inline T sigmoidPrime(T y) {
-    return y*(1-y);
-  }
+	static inline T sigmoidPrime(T y) {
+		return y*(1-y);
+	}
 
-  static inline T tanH(T x) {
-    return 2 * sigmoid(2*x) - 1;
-  }
+	static inline T tanH(T x) {
+		return 2 * sigmoid(2*x) - 1;
+	}
 
-  static inline T tanHPrime(T y) {
-    return 1 / (1 - y * y);
-  }
+	static inline T tanHPrime(T y) {
+		return 1 / (1 - y * y);
+	}
 };
 
 
@@ -71,12 +71,12 @@ class NeuralNetwork : public Approximator<T> {
 		std::vector<Matrix<T>> biases;
 		size_t input_size;
 		size_t output_size;
-    Activation activationType;
+		Activation activationType;
 
 	public:
 
 		NeuralNetwork(std::vector<size_t> sizes, Activation type) {
-      activationType = type;
+			activationType = type;
 			input_size = sizes[0];
 			output_size = *sizes.rbegin();
 			for (auto p = sizes.begin(), c = sizes.begin() + 1; c < sizes.end(); c++, p++) {
@@ -87,11 +87,8 @@ class NeuralNetwork : public Approximator<T> {
 
 		Matrix<T> evaluate(Matrix<T> m){
 			for (auto w = weights.begin(), b = biases.begin(); w < weights.end(); w++, b++){
-        std::cout << m << std::endl;
-        std::cout << "before: "<< m.rows << "x" << m.columns << std::endl;
-        m = (*w ^ m) + *b
-        std::cout << m.rows << "x" << m.columns << std::endl;
-      }
+				m = Activator<T>::activation((*w ^ m) + *b, type);
+			}
 			return m;
 		}
 

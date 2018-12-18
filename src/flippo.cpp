6be927ce6e8ce732ecb69755a400998f0a0f424a
Sven Holtrop <yadeses@gmail.com>
2018-12-18 22:17:34 +0100

    Moved things to gamestate

--- src/flippo.cpp
+++ src/flippo.cpp
@@ -14,29 +14,148 @@ const int BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT;
 
 class GameState {
 public:
-  Matrix<double> board;
+	Matrix<double> board;
+	int moves;
 
-  GameState() {
-    int middle_x = BOARD_WIDTH/2-1;
-    int middle_y = BOARD_HEIGHT/2-1;
-    board = Matrix<double>(BOARD_HEIGHT, BOARD_WIDTH);
-    board(middle_y, middle_x) = 1;
-    board(middle_y+1, middle_x + 1) = 1;
-    board(middle_y, middle_x + 1) = -1;
-    board(middle_y+1, middle_x) = -1;
-  }
+	GameState() {
+		int middle_x = BOARD_WIDTH/2-1;
+		int middle_y = BOARD_HEIGHT/2-1;
+		board = Matrix<double>(BOARD_HEIGHT, BOARD_WIDTH);
+		board(middle_y, middle_x) = 1;
+		board(middle_y+1, middle_x + 1) = 1;
+		board(middle_y, middle_x + 1) = -1;
+		board(middle_y+1, middle_x) = -1;
+		moves = 4;
+	}
+	
+	void placePiece(const int i, const int j, const double c) {
+		board(i, j) = c;
+		// Loop over directions
+		for (int di = -1; di < 2; di++) {
+			for (int dj = -1; dj < 2; dj++) {
+				if (di == 0 && dj == 0) {
+					continue;
+				}
+				update(i, j, di, dj, c);
+			}
+		}
+		moves++;
+	}
 
-  double score() const {
-    double result = 0;
-    for (int i = 0; i < BOARD_SIZE; i++) {
-      result += board[i];
-    }
-    return result;
-  }
+	double score() const {
+		double result = 0;
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			result += board[i];
+		}
+		return result;
+	}
 
-  std::string toString() const {
-    return board.toString();
-  }
+	void update(const int i, const int j, const int di, const int dj, const double c) {
+		// Update along a certain direction given by di, dj
+		auto [fi, fj] = getFlip(i, j, di, dj, c);
+		if (fi == i && fj == j) {
+			return;
+		}
+		fi -= di;
+		fj -= dj;
+		while (fi != i || fj != j) {
+			board(fi, fj) *= -1.0;
+			fi -= di;
+			fj -= dj;
+		}
+	}
+
+	std::vector<std::tuple<int, int>> getNeighbours(const int i, const int j) const {
+
+		std::vector<std::tuple<int, int>> neighbours;
+
+		for (int ni = i - 1; ni < i + 2; ni++) 
+			for (int nj = j - 1; nj < j + 2; nj++) 
+				if (inbound(ni, nj) && board(ni, nj) != 0) 
+			neighbours.push_back({ni, nj});
+
+		return neighbours;
+
+	}
+
+	bool hasNeighbours(const int i, const int j) const {
+		
+	 for (int ni = i - 1; ni < i + 2; ni++) 
+			for (int nj = j - 1; nj < j + 2; nj++) 
+				if (inbound(ni, nj) && board(ni, nj) != 0) 
+			return true;
+
+		return false;
+	}
+
+	inline bool inbound(const int i, const int j) const {
+		return (0 <= i) && (i <	BOARD_HEIGHT) && (0 <= j) && (j < BOARD_WIDTH);
+	}
+
+	std::tuple<int, int> getFlip(int i, int j, const int di, const int dj, const double c) const {
+
+		// Find flip of color along direction di, dj
+		i += di; j += dj;
+		int fi = i, fj = j;
+
+		while (inbound(i, j)) {
+
+			if (board(i, j) == 0) break;
+			if (board(i, j) == c) {fi = i; fj = j;}
+			i += di; j += dj;
+
+		}
+
+		return {fi, fj};
+	
+	}
+
+	int valid(const int i, const int j, const double c) const {
+
+		if (board(i, j) != 0) return 0;
+
+		if (!hasNeighbours(i, j)) return 0;
+
+		for (int di = -1; di < 2; di++) {
+
+			for (int dj = -1; dj < 2; dj++) {
+
+				if (di == 0 && dj == 0) continue;
+				auto [fi, fj] = getFlip(i, j, di, dj, c);
+				if ((fi != i || fj != j) && (fi != i + di || fj != j + dj)) return 2;
+
+			}
+		}
+
+		return 1;
+	}
+
+	Matrix<int> validMoves(const double c) const {
+		Matrix<int> move_matrix(BOARD_HEIGHT, BOARD_WIDTH);
+		for (int i = 0; i < BOARD_WIDTH; i++) {
+			for (int j = 0; j < BOARD_HEIGHT; j++) {
+				move_matrix(i, j) = valid(i, j, c);
+			}
+		}
+		return move_matrix;
+	}
+
+	GameState potentialBoard(const int i, const int j, const double c) const {
+
+		GameState copy(*this);
+		copy.placePiece(i, j, c);
+		return copy;
+
+	}
+
+	inline bool isFinal() const {
+		return moves == BOARD_SIZE;
+	}
+
+
+	std::string toString() const {
+		return board.toString();
+	}
 };
 
 std::ostream& operator<<(std::ostream& os, const GameState& m) {
@@ -44,125 +163,6 @@ std::ostream& operator<<(std::ostream& os, const GameState& m) {
 };
 
 class Flippo {
-public:
-  GameState state;
-  int moves = 4;
 
-  void place_piece(const int i, const int j, const double c) {
-    state.board(i, j) = c;
-    // Loop over directions
-    for (int di = -1; di < 2; di++) {
-      for (int dj = -1; dj < 2; dj++) {
-        if (di == 0 && dj == 0) {
-          continue;
-        }
-        update(i, j, di, dj, c);
-      }
-    }
-    moves++;
-  }
-
-  void update(const int i, const int j, const int di, const int dj, const double c) {
-    // Update along a certain direction given by di, dj
-    auto [fi, fj] = find_flip(i, j, di, dj, c);
-    if (fi == i && fj == j) {
-      return;
-    }
-    fi -= di;
-    fj -= dj;
-    while (fi != i || fj != j) {
-      state.board(fi, fj) *= -1.0;
-      fi -= di;
-      fj -= dj;
-    }
-  }
-
-  std::vector<std::tuple<int, int>> get_neighbours(const int i, const int j) const {
-    std::vector<std::tuple<int, int>> neighbours;
-    for (int di = -1; di < 2; di++) {
-      for (int dj = -1; dj < 2; dj++) {
-        int ni = i + di;
-        int nj = j + dj;
-        if (inbound(ni, nj)) {
-          if (state.board(ni, nj) != 0){
-            neighbours.push_back({ni, nj});
-          }
-        }
-      }
-    }
-    return neighbours;
-  }
-
-  inline bool inbound(const int i, const int j) const {
-    return (0 <= i) && (i <  BOARD_HEIGHT) && (0 <= j) && (j < BOARD_WIDTH);
-  }
-
-  std::tuple<int, int> find_flip(int i, int j, const int di, const int dj, const double c) const {
-    // Find flip of color along direction di, dj
-    i += di;
-    j += dj;
-    int fi = i + di;
-    int fj = j + dj;
-    while (inbound(i, j)) {
-      if (state.board(i, j) == 0) {
-        break;
-      }
-      if (state.board(i, j) == c) {
-        fi = i;
-        fj = j;
-      }
-      i += di;
-      j += dj;
-    }
-    return {fi, fj};
-  }
-
-  GameState potential_board(const int i, const int j, const double c) const {
-    Flippo copy(*this);
-    copy.place_piece(i, j, c);
-    return copy.state;
-  }
-
-  int valid(const int i, const int j, const double c) const {
-    if (state.board(i, j) != 0) {
-      return 0;
-    }
-    std::vector<std::tuple<int, int>> neighbours = get_neighbours(i, j);
-    if (neighbours.size() == 0) {
-      return 0;
-    }
-    for (int di = -1; di < 2; di++) {
-      for (int dj = -1; dj < 2; dj++) {
-        if (di == 0 && dj == 0) {
-          continue;
-        }
-        auto [fi, fj] = find_flip(i, j, di, dj, c);
-        std::cout << fi << " " << fj << " " << i << " " << j << std::endl;
-        if ((fi != i || fj != j) && (fi != i + di || fj != j + dj)) {
-          std::cout << "Found a move" << std::endl;
-          return 2;
-        }
-      }
-    }
-    return 1;
-  }
-
-  Matrix<int> valid_moves(const double c) const {
-    Matrix<int> move_matrix(BOARD_HEIGHT, BOARD_WIDTH);
-    for (int i = 0; i < BOARD_WIDTH; i++) {
-      for (int j = 0; j < BOARD_HEIGHT; j++) {
-        move_matrix(i, j) = valid(i, j, c);
-      }
-    }
-    return move_matrix;
-  }
-
-  inline double score() const {
-    return state.score();
-  }
-
-  inline bool is_final() const {
-    return moves == BOARD_SIZE;
-  }
 };
 #endif
--- unit_tests/src/test_game.cpp
+++ unit_tests/src/test_game.cpp
@@ -5,15 +5,15 @@ using namespace std;
 
 int main() {
   cout << "Testing Flippo" << endl;
-  auto m = Flippo();
-  cout << m.state.board << endl << "Score" << endl << m.state.score() << endl;
+  GameState m;
+  cout << m << endl << "Score" << endl << m.score() << endl;
   cout << "Placing a piece at [2, 4]" << endl;
-  m.place_piece(4, 2, 1.0);
-  cout << m.state.board << endl;
+  m.placePiece(4, 2, 1.0);
+  cout << m << endl;
   // cout << "Testing potential board" << endl;
   // GameState next = m.potential_board(4, 1, 1);
   // cout << next << endl;
   cout << "Testing valid moves" << endl;
-  Matrix<int> possible_moves = m.valid_moves(-1);
+  Matrix<int> possible_moves = m.validMoves(-1);
   cout << possible_moves << endl;
 }
